dde940cb9b3bdbeaff807cbfdbdb4c40e81843ab
Kajanthuka <kajanthu18@gmail.com>
2024-02-24 11:19:31 +0000

    code ammend to accuracy

--- knn_accuracy.py
+++ knn_accuracy.py
@@ -7,7 +7,21 @@ from sklearn.datasets import load_iris
 #Loading data
 irisData = load_iris()
 
-#Split data
+#create feature and target arrays
+X = irisData.data
+Y = irisData.target
+
+#split into training and test data
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.2, random_state=42)
+
+#Model fit
+
+knn = KNeighborsClassifier(n_neighbors=7)
+knn.fit(X_train, Y_train)
+
+# Predict on dataset which model has not seen before
+print(knn.predict(X_test))
+
 
 
 
