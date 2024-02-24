529bda57546829213eccec27289e1ce11f5d6086
Kajanthuka <kajanthu18@gmail.com>
2024-02-24 11:09:35 +0000

    KNN model accuracy

---
+++ knn_accuracy.py
@@ -0,0 +1,13 @@
+#Import necessary modules
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_iris
+
+#Loading data
+irisData = load_iris()
+
+
+
+
+
